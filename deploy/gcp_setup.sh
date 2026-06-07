752896d1cf153d8c5ff4dfd3e4a632ff1388f582
yumari913-cpu <yumari913@gmail.com>
2026-06-07 20:02:37 +0900

    Add Threads Cloud Scheduler deployment

--- deploy/gcp_setup.sh
+++ deploy/gcp_setup.sh
@@ -6,7 +6,7 @@ if [[ -z "${PROJECT_ID:-}" ]]; then
   exit 1
 fi
 
-REGION="${REGION:-asia-northeast1}"
+REGION="${REGION:-us-central1}"
 SERVICE="${SERVICE:-tabelog-instagram}"
 BUCKET="${BUCKET:-${PROJECT_ID}-tabelog-instagram-media}"
 REVIEWER_URL="${REVIEWER_URL:-https://tabelog.com/rvwr/018712231/}"
@@ -40,8 +40,12 @@ gcloud run deploy "${SERVICE}" \
   --image "${IMAGE}" \
   --region "${REGION}" \
   --allow-unauthenticated \
-  --set-env-vars "TABELOG_REVIEWER_URL=${REVIEWER_URL},STORAGE_BUCKET=${BUCKET},PUBLIC_BASE_URL=https://storage.googleapis.com/${BUCKET},AUTO_PUBLISH_FEED=${AUTO_PUBLISH_FEED:-false},AUTO_PUBLISH_STORY=${AUTO_PUBLISH_STORY:-false},AUTO_PUBLISH_REEL=${AUTO_PUBLISH_REEL:-false}" \
-  --set-secrets "IG_USER_ID=ig-user-id:latest,IG_ACCESS_TOKEN=ig-access-token:latest,SYNC_TOKEN=sync-token:latest"
+  --min-instances 0 \
+  --cpu 1 \
+  --memory 512Mi \
+  --concurrency 80 \
+  --set-env-vars "TABELOG_REVIEWER_URL=${REVIEWER_URL},STORAGE_BUCKET=${BUCKET},PUBLIC_BASE_URL=https://storage.googleapis.com/${BUCKET},AUTO_PUBLISH_FEED=${AUTO_PUBLISH_FEED:-false},AUTO_PUBLISH_STORY=${AUTO_PUBLISH_STORY:-false},AUTO_PUBLISH_REEL=${AUTO_PUBLISH_REEL:-false},THREADS_AUTO_PUBLISH=${THREADS_AUTO_PUBLISH:-false},THREADS_AUTO_REPLY=${THREADS_AUTO_REPLY:-false},THREADS_POSTS_PER_DAY=${THREADS_POSTS_PER_DAY:-10},THREADS_INSTAGRAM_PROFILE_URL=${THREADS_INSTAGRAM_PROFILE_URL:-https://www.instagram.com/mogmogtro112233/}" \
+  --set-secrets "IG_USER_ID=ig-user-id:latest,IG_ACCESS_TOKEN=ig-access-token:latest,SYNC_TOKEN=sync-token:latest,THREADS_USER_ID=threads-user-id:latest,THREADS_ACCESS_TOKEN=threads-access-token:latest"
 
 SERVICE_URL="$(gcloud run services describe "${SERVICE}" --region "${REGION}" --format='value(status.url)')"
 
@@ -49,6 +53,7 @@ if gcloud scheduler jobs describe "${SERVICE}-sync" --location "${REGION}" >/dev
   gcloud scheduler jobs update http "${SERVICE}-sync" \
     --location "${REGION}" \
     --schedule "0 * * * *" \
+    --time-zone "Asia/Tokyo" \
     --uri "${SERVICE_URL}/sync" \
     --http-method POST \
     --headers "X-Sync-Token=${SYNC_TOKEN_VALUE:-CHANGE_ME_IN_CONSOLE}"
@@ -56,7 +61,44 @@ else
   gcloud scheduler jobs create http "${SERVICE}-sync" \
     --location "${REGION}" \
     --schedule "0 * * * *" \
+    --time-zone "Asia/Tokyo" \
     --uri "${SERVICE_URL}/sync" \
+    --http-method POST \
+    --headers "X-Sync-Token=${SYNC_TOKEN_VALUE:-CHANGE_ME_IN_CONSOLE}"
+fi
+
+if gcloud scheduler jobs describe "${SERVICE}-threads-tick" --location "${REGION}" >/dev/null 2>&1; then
+  gcloud scheduler jobs update http "${SERVICE}-threads-tick" \
+    --location "${REGION}" \
+    --schedule "0 * * * *" \
+    --time-zone "Asia/Tokyo" \
+    --uri "${SERVICE_URL}/threads/tick" \
+    --http-method POST \
+    --headers "X-Sync-Token=${SYNC_TOKEN_VALUE:-CHANGE_ME_IN_CONSOLE}"
+else
+  gcloud scheduler jobs create http "${SERVICE}-threads-tick" \
+    --location "${REGION}" \
+    --schedule "0 * * * *" \
+    --time-zone "Asia/Tokyo" \
+    --uri "${SERVICE_URL}/threads/tick" \
+    --http-method POST \
+    --headers "X-Sync-Token=${SYNC_TOKEN_VALUE:-CHANGE_ME_IN_CONSOLE}"
+fi
+
+if gcloud scheduler jobs describe "${SERVICE}-threads-engage" --location "${REGION}" >/dev/null 2>&1; then
+  gcloud scheduler jobs update http "${SERVICE}-threads-engage" \
+    --location "${REGION}" \
+    --schedule "30 9-22/2 * * *" \
+    --time-zone "Asia/Tokyo" \
+    --uri "${SERVICE_URL}/threads/engage" \
+    --http-method POST \
+    --headers "X-Sync-Token=${SYNC_TOKEN_VALUE:-CHANGE_ME_IN_CONSOLE}"
+else
+  gcloud scheduler jobs create http "${SERVICE}-threads-engage" \
+    --location "${REGION}" \
+    --schedule "30 9-22/2 * * *" \
+    --time-zone "Asia/Tokyo" \
+    --uri "${SERVICE_URL}/threads/engage" \
     --http-method POST \
     --headers "X-Sync-Token=${SYNC_TOKEN_VALUE:-CHANGE_ME_IN_CONSOLE}"
 fi
